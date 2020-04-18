b97edd246f2aab2ce74505d1feaebf7e9bba2d74
Matt <mattwarrenmail@gmail.com>
2020-04-17 22:34:26 -0700

    friday

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/webhook', (req, res) => {
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-      console.log('entry is', entry)
+      console.log('entry is', JSON.stringify(entry))
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
